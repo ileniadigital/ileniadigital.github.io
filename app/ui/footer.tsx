bc1ee8f04467ca1bbad2b9d8358973faa54c7428
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-06-29 15:16:13 +0100

    set nav and footer

---
+++ app/ui/footer.tsx
@@ -0,0 +1,8 @@
+// Footer component
+export default function Footer() {
+    return (
+        <footer>
+            <p>© 2024 Ilenia Maietta</p>
+        </footer>
+    );
+}
---
+++ app/ui/nav.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+// Navigation component
+export default function Nav() {
+    return (
+        <nav className="flex flex-row justify-center items-center ">
+            {/* Logo */}
+            <Image src="/images/branding/logo.png" alt="Blue and yellow logo of letter I and M" width={50} height={50} />
+            {/* Navigational menu */}
+            <ul className="flex flex-row gap-10">
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/projects">Projects</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
