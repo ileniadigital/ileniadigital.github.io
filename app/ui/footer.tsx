0db13265c283eb045674cdb9ceeb2e1cd82540ca
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-03 20:02:37 +0100

    styled footer

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,7 +1,7 @@
 // Footer component
 export default function Footer() {
     return (
-        <footer>
+        <footer className="flex justify-center text-white font bg-darkgreen p-2">
             <p>© 2024 Ilenia Maietta</p>
         </footer>
     );
