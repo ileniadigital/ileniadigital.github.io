8958aac9968d408dffa7ad971688505dc6fa1e03
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-04 00:20:06 +0100

    added favicon(not displaying yet)

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,6 @@ import Nav from './ui/nav';
 import Footer from './ui/footer';
 import Hero from './ui/home/hero';
 
-const name = [
-  {
-    text: "Ilenia",
-    className: "text-darkgreen",
-  },
-  {
-    text: "Maietta",
-    className: "text-darkgreen", // Add this class if needed
-  },
-];
-
-const jobs = [
-  {
-    text: "Frontend",
-    className: "text-blue-500", // Add this class if needed
-  },
-  {
-    text: "Developer",
-    className: "text-darkgreen", // Add this class if needed
-  },
-];
-
 export default function Home() {
   return (
     <div className='bg-offwhite'>
