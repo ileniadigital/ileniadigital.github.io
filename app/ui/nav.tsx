5bfab2c89658cd77ac348237cf793349a364daa7
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-03 19:56:42 +0100

    Added stylign for hovering over navigation items

--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -7,18 +7,18 @@ export default function Nav() {
             {/* Logo */}
             <Image src="/images/branding/logo.png" alt="Blue and yellow logo of letter I and M" width={50} height={50} />
             {/* Navigational menu */}
-            <ul className="flex flex-row gap-10 ml-auto mr-auto text-white text-lg bg-slate-700 p-4 rounded-full mt-3">
+            <ul className="flex flex-row gap-10 ml-auto mr-auto text-white text-lg bg-darkgreen p-5 rounded-full mt-3">
                 <li>
-                    <a href="/">Home</a>
+                    <a href="/" className="hover:text-yellow hover:font-bold">Home</a>
                 </li>
                 <li>
-                    <a href="/about">About</a>
+                    <a href="/about" className="hover:text-yellow hover:font-bold">About</a>
                 </li>
                 <li>
-                    <a href="/projects">Projects</a>
+                    <a href="/projects" className="hover:text-yellow hover:font-bold">Projects</a>
                 </li>
                 <li>
-                    <a href="/contact">Contact</a>
+                    <a href="/contact" className="hover:text-yellow hover:font-bold">Contact</a>
                 </li>
             </ul>
         </nav>
