89fa8cd4a90cb71ee39f5c1816cfc1eba0e4aeec
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-03 18:55:12 +0100

    set theme colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        darkgreen: "#005D3D",
+        lightgreen: "#29BF12",
+        yellow: "#F9CC00",
+        lightblue: "#08BDBD",
+        pink: "#F21B3F"
+      }
     },
   },
   plugins: [],
