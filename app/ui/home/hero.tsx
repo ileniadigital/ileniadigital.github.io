c1c2fd20b470e77a345ebb78f453d9eb2e768b1d
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-03 23:34:15 +0100

    added typewriting animation

--- app/ui/home/hero.tsx
+++ app/ui/home/hero.tsx
@@ -3,45 +3,54 @@ import { TypewriterEffect } from './type-writer-effect';
 
 const name = [
     {
-        text: "Ilenia",
-        className: "text-darkgreen",
+        text: "Amazon",
+        className: "!text-black text-3xl",
     },
     {
-        text: "Maietta",
-        className: "text-darkgreen",
-    },
-];
-
-const jobs = [
-    {
-        text: "Frontend",
-        className: "text-blue-500",
+        text: "Future",
+        className: "!text-black text-3xl",
     },
     {
-        text: "Developer",
-        className: "text-darkgreen",
+        text: "Engineer.",
+        className: "!text-black text-3xl",
+    },
+    {
+        text: "Software",
+        className: "!text-black text-3xl",
+    },
+    {
+        text: "Engineer.",
+        className: "!text-black text-3xl",
+    },
+    {
+        text: "Content",
+        className: "!text-black text-3xl",
+    },
+    {
+        text: "Creator.",
+        className: "!text-black text-3xl",
     },
 ];
 
 export default function Hero() {
     return (
-        <div>
+        <div >
             <main>
                 {/* Hero section */}
                 <div className='flex flex-row'>
-                    <div className='flex flex-col gap-5'>
-                        <h1 className='text-5xl'>Hi I am Ilenia!</h1>
-                        <h2 className='text-2xl'>Amazon Future Engineer. Software Developer. Content Creator</h2>
+                    <div className='flex flex-col gap-5 mt-40 ml-20'>
+                        <h2 className="text-7xl font-semibold">Hi, I am Ilenia!</h2>
+                        <h3><TypewriterEffect words={name} /></h3>
                         {/* Programming languages icons */}
                         <div>
 
                         </div>
-                        <button className='w-48 rounded-full bg-darkgreen p-3 text-white hover:text-yellow'><a href="/projects">See my projects</a></button>
+                        <button className='w-48 rounded-full bg-darkgreen p-3 text-white hover:text-yellow hover:font-bold hover:text-lg'><a href="/projects">See my projects</a></button>
                     </div>
 
                     {/* Portrait */}
-                    <figure>
-                        <Image src='/images/portrait.png' alt="yellow, green and pink portrait of Ilenia" width={300} height={900}></Image>
+                    <figure className='mr-auto ml-32 '>
+                        <Image src='/images/portrait_no_bckg.png' alt="yellow, green and pink portrait of Ilenia" width={400} height={1200}></Image>
                     </figure>
 
                 </div >
