a906e7f9e8cad05818ab5eb8867a331f43f00aa8
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-03 18:55:00 +0100

    fixed nav

--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 // Navigation component
 export default function Nav() {
     return (
-        <nav className="flex flex-row justify-center items-center bg-slate-800">
+        <nav className="flex flex-row items-center w-full">
             {/* Logo */}
-            <Image src="/images/branding/logo.png" alt="Blue and yellow logo of letter I and M" width={50} height={50} className="items-start" />
+            <Image src="/images/branding/logo.png" alt="Blue and yellow logo of letter I and M" width={50} height={50} />
             {/* Navigational menu */}
-            <ul className="flex flex-row gap-10 ml-20">
+            <ul className="flex flex-row gap-10 ml-auto mr-auto text-white text-lg bg-slate-700 p-4 rounded-full mt-3">
                 <li>
                     <a href="/">Home</a>
                 </li>
