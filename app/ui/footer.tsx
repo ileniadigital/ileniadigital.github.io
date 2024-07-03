960e713252d7b3b803a4428bfbbab0c7e4bf06d4
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-03 23:55:39 +0100

    fixed footer

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,8 +1,25 @@
 // Footer component
 export default function Footer() {
     return (
-        <footer className="flex justify-center text-white font bg-darkgreen p-2">
+        <footer className="flex flex-row items-center w-full text-xl text-white font bg-darkgreen p-2">
             <p>© 2024 Ilenia Maietta</p>
+            <nav className="ml-auto">
+                {/* Navigational menu */}
+                <ul className="flex flex-row gap-10 ml-auto mr-5 text-white text-lg">
+                    <li>
+                        <a href="/" className="hover:text-xl hover:font-bold">Home</a>
+                    </li>
+                    <li>
+                        <a href="/about" className="hover:text-xl hover:font-bold">About</a>
+                    </li>
+                    <li>
+                        <a href="/projects" className="hover:text-xl hover:font-bold">Projects</a>
+                    </li>
+                    <li>
+                        <a href="/contact" className="hover:text-xl hover:font-bold">Contact</a>
+                    </li>
+                </ul>
+            </nav>
         </footer>
     );
 }
