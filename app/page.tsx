f82c726f5f7d46c59ad7fdca9df56075bec99c7e
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-03 21:43:27 +0100

    imported images for hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,56 @@
 import Image from 'next/image';
-
-// Import UI components
 import Nav from './ui/nav';
 import Footer from './ui/footer';
+import { TypewriterEffect } from './ui/home/type-writer-effect';
+
+const name = [
+  {
+    text: "Ilenia",
+    className: "text-darkgreen",
+  },
+  {
+    text: "Maietta",
+    className: "text-darkgreen", // Add this class if needed
+  },
+];
+
+const jobs = [
+  {
+    text: "Frontend",
+    className: "text-blue-500", // Add this class if needed
+  },
+  {
+    text: "Developer",
+    className: "text-darkgreen", // Add this class if needed
+  },
+];
 
 export default function Home() {
   return (
-    <body>
+    <div>
       <Nav />
       <main>
-        <div>
-          <h1>My portfolio</h1>
-        </div>
-      </main>
+        {/* Hero section */}
+        <div className='flex flex-row bg-yellow'>
+          <div>
+            <h1>Hi I am Ilenia!</h1>
+            <h2>Amazon Future Engineer. Front-end Developer. Freelancer</h2>
+            {/* Programming languages icons */}
+            <div>
+
+            </div>
+            <button><a href="/projects">See my projects</a></button>
+          </div>
+
+          {/* Portrait */}
+          <figure>
+            <Image src='/images/portrait.png' alt="yellow, green and pink portrait of Ilenia" width={100} height={300}></Image>
+          </figure>
+
+        </div >
+
+      </main >
       <Footer />
-    </body >
-
-
-
-  )
+    </div >
+  );
 }
