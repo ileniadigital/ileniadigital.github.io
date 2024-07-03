5471dd5c1a0df8350764459344e2419359afc244
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-04 00:02:32 +0100

    increased font size for nav

--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -11,18 +11,18 @@ export default function Nav() {
             </div>
 
             {/* Navigational menu */}
-            <ul className="flex flex-row gap-10 ml-auto mr-5 text-darkgreen text-lg">
+            <ul className="flex flex-row gap-10 ml-auto mr-5 text-darkgreen text-xl">
                 <li>
-                    <a href="/" className="hover:text-xl hover:font-bold">Home</a>
+                    <a href="/" className="hover:text-2xl hover:font-bold">Home</a>
                 </li>
                 <li>
-                    <a href="/about" className="hover:text-xl hover:font-bold">About</a>
+                    <a href="/about" className="hover:text-2xl hover:font-bold">About</a>
                 </li>
                 <li>
-                    <a href="/projects" className="hover:text-xl hover:font-bold">Projects</a>
+                    <a href="/projects" className="hover:text-2xl hover:font-bold">Projects</a>
                 </li>
                 <li>
-                    <button><a href="/contact" className="bg-darkgreen text-yellow rounded-full p-3 hover:text-xl hover:font-bold">Contact</a></button>
+                    <button><a href="/contact" className="bg-darkgreen text-yellow rounded-full p-3 hover:text-2xl hover:font-bold">Contact</a></button>
                 </li>
             </ul>
         </nav>
