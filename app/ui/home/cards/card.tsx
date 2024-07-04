08839ac5d5217b327393bab1f45d531ff350d057
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-04 17:56:37 +0100

    base styling for language cards

--- app/ui/home/cards/card.tsx
+++ app/ui/home/cards/card.tsx
@@ -1,16 +1,15 @@
 import Image from 'next/image';
 
 interface CardProps {
-    logo: string;
-    name: string;
+    language: string;
 }
 
 // Card component
-export default function Card({ logo, name }: CardProps) {
+export default function Card({ language }: CardProps) {
     return (
-        <div className='w-24 p-1 rounded-lg border-4 border-darkgreen'>
-            <Image src={`/images/languages/${logo}.png`} alt={name} width={100} height={100} />
-            <h5>{name}</h5>
+        <div className='w-24 p-1 rounded-lg border-4 border-darkgreen flex flex-col gap-3 justify-center items-center'>
+            <Image src={`/images/languages/${language}.png`} alt={language} width={100} height={100} />
+            <h5>{language}</h5>
         </div>
     );
 }
--- app/ui/home/languages.tsx
+++ app/ui/home/languages.tsx
@@ -1,11 +1,28 @@
 import Image from "next/image";
 import Card from "./cards/card";
 
+const languages = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "php",
+    "TypeScript",
+    "Python",
+    "Java",
+    "Next",
+    "React",
+    "Tailwind",
+]
+
 export default function Languages() {
     return (
         <section>
-            <h4>Programming Languages</h4>
-            <Card logo={"html"} name={"HTML"} />
+            <h4 className="text-darkgreen text-3xl font-bold">Programming Languages</h4>
+            <div className='flex flex-row gap-16 justify-center'>
+                {languages.map((language, index) => (
+                    <Card key={index} language={language} />
+                ))}
+            </div>
         </section >
     );
 }
