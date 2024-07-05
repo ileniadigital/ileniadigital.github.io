9332af4631ee927d5a36126c4f50ba78fda8aa71
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-05 18:30:35 +0100

    drafted education section

---
+++ app/ui/about/education.tsx
@@ -0,0 +1,16 @@
+export default function Education() {
+    return (
+        <section>
+            <h1>Education</h1>
+            <h2>Queen Mary University of London 2022-2026</h2>
+            <p>Information about univeristy</p>
+            <h3>Modules studied</h3>
+            <ul>
+                <li></li>
+                <li>Web Development</li>
+                <li>Database Systems</li>
+                <li>Computer Systems</li>
+            </ul>
+        </section>
+    );
+}
