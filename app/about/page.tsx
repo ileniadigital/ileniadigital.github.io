5b19b409651416fa7e6746de91d9938b6e7e6a16
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-07 16:31:36 +0100

    added picture of me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 // Import UI components
 import Nav from '../ui/nav';
 import Footer from '../ui/footer';
@@ -12,12 +13,16 @@ export default function About() {
             <main className='flex flex-row mt-5'>
                 <Aside />
                 <div>
-                    <div className="ml-5">
-                        <h1 className='text-3xl font-bold'>About me</h1>
-                        <p className="mt-3 text-lg">
-                            I am originally from a small town in South Italy, and moved to the UK in 2018. I am a Computer Science Student at Queen Mary University of London, with a passion for development and design.
-                            Other than a tech lover, I am an amateaur boxer, a bookworm and a passionate crocheter.
-                        </p>
+                    <div className="ml-5 flex flex-row">
+                        <div className='mt-10'>
+                            <h1 className='text-3xl font-bold'>About me</h1>
+                            <p className="mt-3 text-xl">
+                                I am originally from a small town in South Italy, and moved to the UK in 2018. I am a Computer Science Student at Queen Mary University of London, with a passion for development and design.
+                                Other than a tech lover, I am an amateaur boxer, a bookworm and a passionate crocheter.
+                            </p>
+                        </div>
+                        <Image src={'/images/ilenia.jpg'} alt={"Picture of Ilenia in front of a waterfall"} width={400} height={1200} className="mr-44 rounded-3xl" />
+                        {/* ADD WIDGETS OF HOBBIES??? */}
                     </div>
                     <Education />
                     <Experience />
