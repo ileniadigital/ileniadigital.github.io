c34bc824a332e2fdd1eaf368c411f0f7103baa60
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-07 16:41:39 +0100

    adjusted education section

--- app/ui/about/education.tsx
+++ app/ui/about/education.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+//Import UI Components
 import Cards from "../cards/cards";
 
 const modules = [
@@ -21,12 +23,15 @@ const modules = [
 // Education component with details of education
 export default function Education() {
     return (
-        <section className="mt-5">
+        <section className="mt-5 bg-main">
             <div className="ml-5">
-                <h1 className='text-3xl font-bold'>Education</h1>
+                <h1 className='text-3xl font-bold pt-5'>Education</h1>
                 <h2 className='text-2xl font-bold mt-3'>MSci Computer Science</h2>
-                <h2 className='text-2xl font-bold mt-2'>Queen Mary University of London (2022-2026)</h2>
-                <p>Information about university</p>
+                {/* University and Logo */}
+                <div className='flex flex-row items-center gap-3 mt-3'>
+                    <Image src={'/images/work/qmul.jpeg'} alt={"Queen Mary University of London Logo"} width={50} height={50} />
+                    <h2 className='text-2xl font-bold mt-2'>Queen Mary University of London (2022-2026)</h2>
+                </div>
             </div>
             <Cards elements={modules} title="Modules studied" image={false} autofit />
         </section>
