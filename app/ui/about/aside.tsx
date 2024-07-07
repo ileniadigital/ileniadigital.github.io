26c9676ded668f531b9cbe364272e2c78f8a8f04
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-07 16:32:10 +0100

    adjusted aside length to cover all page

--- app/ui/about/aside.tsx
+++ app/ui/about/aside.tsx
@@ -1,6 +1,6 @@
 export default function Aside() {
     return (
-        <aside className="w-1/4 bg-main h-screen">
+        <aside className="w-1/4 bg-main h-auto">
             <ul className="flex flex-col gap-4 text-white text-xl fixed ml-3 mt-7">
                 <li className="hover:text-2xl hover:font-bold">
                     <a>About me</a>
