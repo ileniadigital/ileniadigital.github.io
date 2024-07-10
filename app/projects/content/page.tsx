7149841bc6e30abe02c40eb9da2c646e8d7c056c
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-10 21:43:58 +0100

    Created content and programming pages

---
+++ app/projects/content/page.tsx
@@ -0,0 +1,21 @@
+import Nav from '../../ui/nav';
+import Footer from '../../ui/footer';
+import Card from '../../ui/projects/card';
+
+export default function Content() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Nav />
+            <main className="flex flex-col justify-center items-center flex-grow gap-32">
+                <h1 className='text-5xl font-bold text-black'>Content Creation</h1>
+                <div className='flex flex-row gap-44 justify-center items-center'>
+                    <Card title="Programming" bg="coding" link="/programming" />
+                    <Card title="Content Creation" bg="content" link="/content" />
+                </div>
+            </main>
+            <div className='mt-auto'>
+                <Footer />
+            </div>
+        </div>
+    )
+}
---
+++ app/projects/programming/page.tsx
@@ -0,0 +1,21 @@
+import Nav from '../../ui/nav';
+import Footer from '../../ui/footer';
+import Card from '../../ui/projects/card';
+
+export default function Programming() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Nav />
+            <main className="flex flex-col justify-center items-center flex-grow gap-32">
+                <h1 className='text-5xl font-bold text-black'>Programming Projects</h1>
+                <div className='flex flex-row gap-44 justify-center items-center'>
+                    <Card title="Swiftsheets" bg="coding" link="/programming/swiftsheets" />
+                    <Card title="Mountain Hikers Weather App" bg="content" link="/programming/weatherapp" />
+                </div>
+            </main>
+            <div className='mt-auto'>
+                <Footer />
+            </div>
+        </div>
+    )
+}
