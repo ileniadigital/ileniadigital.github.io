45e19099550c4aaf7538e788bff3beda547ff4a7
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-10 21:50:00 +0100

    added ffp page

--- app/projects/content/fightforpeace/page.tsx
+++ app/projects/content/fightforpeace/page.tsx
@@ -1,12 +1,12 @@
 import Nav from '../../../ui/nav';
 import Footer from '../../../ui/footer';
 
-export default function SocialMedia() {
+export default function FightForPeace() {
     return (
         <div className="flex flex-col min-h-screen">
             <Nav />
             <main className="flex flex-col justify-center items-center flex-grow gap-32">
-                <h1 className='text-5xl font-bold text-black'>Social Media</h1>
+                <h1 className='text-5xl font-bold text-black'>Fight For Peace</h1>
             </main>
             <div className='mt-auto'>
                 <Footer />
