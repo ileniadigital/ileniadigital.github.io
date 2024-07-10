b012c953800b111fd2872a805c297cf6ed866f74
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-10 21:48:22 +0100

    added cards for content projects

--- app/projects/content/page.tsx
+++ app/projects/content/page.tsx
@@ -9,8 +9,9 @@ export default function Content() {
             <main className="flex flex-col justify-center items-center flex-grow gap-32">
                 <h1 className='text-5xl font-bold text-black'>Content Creation</h1>
                 <div className='flex flex-row gap-44 justify-center items-center'>
-                    <Card title="Programming" bg="coding" link="/programming" />
-                    <Card title="Content Creation" bg="content" link="/content" />
+                    <Card title="Social Media" bg="coding" link="/socialmedia" />
+                    <Card title="Fight For Peace" bg="content" link="/fightforpeace" />
+                    <Card title="Byte Queens" bg="content" link="/bytequeens" />
                 </div>
             </main>
             <div className='mt-auto'>
