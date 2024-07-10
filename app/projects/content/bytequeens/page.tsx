f47e457e22de2e53cd54f2af88e62a9f2f98d0a0
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-10 21:50:38 +0100

    added byte queens page

---
+++ app/projects/content/bytequeens/page.tsx
@@ -0,0 +1,16 @@
+import Nav from '../../../ui/nav';
+import Footer from '../../../ui/footer';
+
+export default function ByteQueens() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Nav />
+            <main className="flex flex-col justify-center items-center flex-grow gap-32">
+                <h1 className='text-5xl font-bold text-black'>Byte Queens</h1>
+            </main>
+            <div className='mt-auto'>
+                <Footer />
+            </div>
+        </div>
+    )
+}
