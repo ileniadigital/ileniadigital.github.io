fedc048050f49ef02d8fe05a60bb44bbde5b773d
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-10 16:23:00 +0100

    cleaned up message component to use variables instead of declaring button twice

--- app/ui/contact/message.tsx
+++ app/ui/contact/message.tsx
@@ -1,21 +1,25 @@
 interface MessageProps {
     message: string;
     email: boolean;
-
 }
+
 export default function Message({ message, email }: MessageProps) {
+    let link = "";
+    let text = "";
+
+    // Render text based on type of link to use
+    if (email) {
+        link = "mailto:ileniamaietta.business@gmail.com";
+        text = "Email me";
+    } else {
+        link = "https://beacons.ai/ilenia.digital";
+        text = "Follow me";
+    }
+
     return (
         <div className="bg-offwhite rounded-3xl h-52 w-52 flex flex-col gap-5 justify-center items-center">
             <h1 className="text-xl font-bold text-center">{message}</h1>
-            {/* Email me button */}
-            {email ? (
-                <button className="btn"><a href="mailto:ileniamaietta.business@gmail.com">Email me</a></button>
-            )
-                // Follow me button
-                : (
-                    <button className="btn"><a href="https://beacons.ai/ilenia.digital">Follow me</a></button>
-                )}
-
+            <button className="btn"><a href={link}>{text}</a></button>
         </div>
-    )
+    );
 }
