5103c4c0d5651d4aaabdde5298e364f08e4eb38d
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-10 21:22:03 +0100

    made project card a button

--- app/ui/projects/card.tsx
+++ app/ui/projects/card.tsx
@@ -6,10 +6,16 @@ interface CardProps {
     bg: string
 }
 export default function Card({ title, bg }: CardProps) {
-    const cardStyle = `bg-${bg} bg-cover bg-center filter w-96 h-72 rounded-xl flex items-center justify-center`;
+    const cardStyle = `bg-${bg} bg-cover bg-center filter w-96 h-72 rounded-xl flex items-center justify-center hover:scale-105 transform transition duration-500 ease-in-out`;
     return (
-        <div className={cardStyle}>
-            <h1 className="text-4xl text-offwhite font-bold">{title}</h1>
+        <div>
+            <button>
+                <a href="/projects">
+                    <div className={cardStyle}>
+                        <h1 className="text-4xl text-offwhite font-bold">{title}</h1>
+                    </div>
+                </a>
+            </button>
         </div>
     );
 }
