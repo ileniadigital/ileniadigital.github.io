86f7f5f4264cbbfc8e45aa02de40c0e8afe6b7b3
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-10 18:48:37 +0100

    added images for background

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,11 @@
-import Image from 'next/image';
+//Background images sources
+//Coding image: Photo by Danny Meneses from Pexels: https://www.pexels.com/photo/photo-of-turned-on-laptop-computer-943096/
+//Content creation image: Photo by Caio  : https://www.pexels.com/photo/turned-on-screen-silver-macbook-air-on-wooden-desk-56759/
 
 // Import UI components
 import Nav from '../ui/nav';
 import Footer from '../ui/footer';
+import Card from '../ui/projects/card';
 
 export default function Projects() {
     return (
@@ -11,6 +14,7 @@ export default function Projects() {
             <main>
                 <div>
                     <h1>My Projects</h1>
+                    <Card title="Programming" image="project1.png" />
                 </div>
             </main>
             <Footer />
