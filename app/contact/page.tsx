4113df3719073be9bd5a7e48e3f199526ea7d62c
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-10 16:18:39 +0100

    chnaged styling of contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,17 +5,18 @@ import Message from '../ui/contact/message';
 
 export default function Contact() {
     return (
-        <div>
+        <div className="flex flex-col min-h-screen">
             <Nav />
-            <main>
+            <main className="flex-grow">
                 {/* Cards */}
-                <div className='flex flex-row gap-40 items-center justify-center mt-20'>
+                <div className='flex flex-row gap-40 items-center justify-center mt-48 bg-main p-20'>
                     <Message message='Want to work together?' email={true} />
                     <Message message='Want to see my work?' email={false} />
                 </div>
-
-                <Footer />
             </main>
+            <div className='mt-auto'>
+                <Footer />
+            </div>
         </div>
     );
 };
