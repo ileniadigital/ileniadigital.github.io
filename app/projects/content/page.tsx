f43d7cd091cd17f14c26fa9166a1314b38424dff
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-10 21:52:44 +0100

    fixed content urls

--- app/projects/content/page.tsx
+++ app/projects/content/page.tsx
@@ -9,9 +9,9 @@ export default function Content() {
             <main className="flex flex-col justify-center items-center flex-grow gap-32">
                 <h1 className='text-5xl font-bold text-black'>Content Creation</h1>
                 <div className='flex flex-row gap-44 justify-center items-center'>
-                    <Card title="Social Media" bg="coding" link="/socialmedia" />
-                    <Card title="Fight For Peace" bg="content" link="/fightforpeace" />
-                    <Card title="Byte Queens" bg="content" link="/bytequeens" />
+                    <Card title="Social Media" bg="coding" link="/content/socialmedia" />
+                    <Card title="Fight For Peace" bg="content" link="/content/fightforpeace" />
+                    <Card title="Byte Queens" bg="content" link="/content/bytequeens" />
                 </div>
             </main>
             <div className='mt-auto'>
