305d9dd8f7200dac68445284d936aed04bd7bc00
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-11 15:36:36 +0100

    uploaded pictures for content, styled

--- app/ui/projects/card.tsx
+++ app/ui/projects/card.tsx
@@ -12,7 +12,7 @@ export default function Card({ title, bg, link }: CardProps) {
     return (
         <div>
             <button>
-                <Link href={link}>
+                <Link href={page}>
                     <div className="bg-main bg-cover bg-center filter w-120 h-96 rounded-xl flex flex-col items-center justify-center hover:scale-105">
                         <Image src={bg} alt={bg} width={450} height={288} className='p-3' />
                         <h1 className="text-xl text-black font-bold truncate">{title}</h1>
