5763c60e28b725f6b52cc4bb397101e61c1b6291
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-07-11 13:13:50 +0100

    added images and descriptions for Swiftsheets and efitter

--- app/data/projectsData.ts
+++ app/data/projectsData.ts
@@ -2,17 +2,21 @@ export const projects={
   programming:[
     {
         id: 'swiftsheets',
-        title: 'Swiftsheets',
-        description: 'Description of Project 1',
+        title: 'Swiftsheets for FDM Group',
+        description: 'I developed a timesheet application for Consultants, Line Managers, System Administrators and Finance Team Members to submit and manage timesheets, and users for the Administrator. I developed it with a team of other students over the course of 6 weeks, suing React.js for the front-end and Django for the back-end.',
         images: [
-          '/images/projects/programming/swiftsheets.png',
+          '/images/projects/programming/swiftsheets/swiftsheets.png',
+          '/images/projects/programming/swiftsheets/events.png',
+          '/images/projects/programming/swiftsheets/settings.png',
+          '/images/projects/programming/swiftsheets/linemanager.png',
+          '/images/projects/programming/swiftsheets/timesheet.png',
         ],
-        languages: ['Next.js', 'TypeScript', 'Django'],
+        languages: ['React.js', 'JavaScript', 'Django'],
       },
       {
         id: 'weatherapp',
         title: 'Mountain Hikers Weather App',
-        description: 'Description of Project 1',
+        description: 'I developed in a team of students a web-based weather application for mountain hikers. The application provides various forecast data and graphical weather data for mountain locations across the globe.',
         images: [
           '/images/projects/programming/weatherapp/desktop_home.png',
           '/images/projects/programming/weatherapp/desktop_location.png',
@@ -22,6 +26,15 @@ export const projects={
           '/images/projects/programming/weatherapp/mobile_menu.png',
           '/images/projects/programming/weatherapp/mobile_search.png',
           '/images/projects/programming/weatherapp/mobile_windmap.png',
+        ],
+        languages: ['React.js', 'JavaScript', 'CSS'],
+      },
+      {
+        id: 'efitter',
+        title: 'eFitter Customer Dashboard',
+        description: 'The B2C dashboard provides a central place for customers to view their sizes across the brands supported by eFitter, to reduce the environmental impact of returns from online shopping.',
+        images: [
+          '/images/projects/programming/efitter.png',
         ],
         languages: ['Next.js', 'TypeScript', 'Django'],
       },
