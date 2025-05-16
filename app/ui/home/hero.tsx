5d76ede7909481de1305fb4e6a6328e3ef08032f
ileniadigital <ileniamaietta.business@gmail.com>
2025-05-17 00:12:22 +0100

    make it scalable for mobile

--- app/ui/home/hero.tsx
+++ app/ui/home/hero.tsx
@@ -46,18 +46,18 @@ export default function Hero() {
             <main className="w-full h-full min-h-screen flex flex-col">
                 <div className='flex flex-row flex-1'>
                     {/* Left column */}
-                    <div className='flex flex-col gap-5 mt-40 ml-20 max-w-xl flex-shrink-0'>
-                        <h2 className="text-7xl font-semibold">Hi, I am Ilenia!</h2>
-                        <h2 className="text-3xl justify-normal ">
+                    <div className="flex flex-col gap-5 mt-20 md:mt-40 ml-5 md:ml-20 max-w-xl flex-shrink-0">
+                        <h2 className="text-5xl md:text-7xl font-semibold">Hi, I am Ilenia!</h2>
+                        <h3 className="text-xl md:text-3xl justify-normal ">
                             I am a Software Engineering Intern at Millennium, studying Computer Science at university. I focus on developing smooth and intuitive user experiences, to render technology suitable for all.
-                        </h2>
-                        <button className='w-56 ml-32 text-xl btn '>
+                        </h3>
+                        <button className="w-full md:w-56 md:ml-32 text-xl btn">
                             <a href="/projects">See my projects</a>
                         </button>
                     </div>
 
                     {/* Right column - Portrait */}
-                    <div className="relative flex-1 h-[500px] md:h-[700px]">
+                    <div className="relative flex-1 h-0 md:h-[700px] md:mt-0 hidden md:block">
                         <Image
                             src='/images/portrait_no_bckg.png'
                             alt="yellow, green and pink portrait of Ilenia"
