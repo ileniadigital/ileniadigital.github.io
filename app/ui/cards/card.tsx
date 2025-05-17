2df34a84ad4d2902f7d18a1b69b1361aa14193f2
ileniadigital <ileniamaietta.business@gmail.com>
2025-05-17 01:25:36 +0100

    added languages (need to add more links and fix layout)

--- app/ui/cards/card.tsx
+++ app/ui/cards/card.tsx
@@ -1,5 +1,6 @@
 interface Module {
     name: string;
+    languages?: string;
     grade: string;
     projectLink?: string;
 }
@@ -13,7 +14,10 @@ export default function Card({ module, autofit }: CardProps) {
     return (
         <div className={`p-3 rounded-lg grid grid-rows-3 bg-offwhite shadow-lg ${autofit ? 'min-w-[20rem]' : 'min-w-[12rem]'}`}>
             <h5 className="text-lg font-semibold mb-2">{module.name}</h5>
-            <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-col justify-between items-center">
+                {module.languages && (
+                    <span className="">Languages: {module.languages}</span>
+                )}
                 <span className="font-bold">Grade: {module.grade}</span>
                 {module.projectLink && (
                     <a
